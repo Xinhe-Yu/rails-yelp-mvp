3f36d8f18a0b0f4525607159a9ebe918a18fd2e1
Xinhe YU <aspasia.yu@gmail.com>
2024-08-07 14:06:27 +0200

    MVC

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,37 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %w[new create destroy]
+  before_action :set_review, only: %w[destroy]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to restaurant_path(@restaurant), status: :see_other
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
